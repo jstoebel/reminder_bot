be230d7af62aa0b345054d10cf9a027d8dc929f7
Jacob Stoebel <jstoebel@gmail.com>
2017-08-22 14:50:02 -0400

    finish with slash commands

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify, request
 from mongoengine import connect, DoesNotExist
 from models import Task
 import json
+import dateparser
 
 app_config = json.loads(open('app.json').read())
 app = Flask(app_config['name'])
@@ -27,13 +28,12 @@ def new_task():
     try:
         # try to set the attributes
         text = request.form['text']
-        regex = 'name=(?P<name>.*)freq=(?P<freq>.*)qty=(?P<freq>.*)units=(?P<units>.*)'
-        match = re.search(, text)
+        regex = 'name=(?P<name>.*)freq=(?P<freq>.*)'
+        match = re.search(regex, text)
         name, freq_str = match.group('name').strip(), match.group('freq')
         frequency = int(freq_str)
         attrs = {'name': name, 'frequency': frequency}
     except (ValueError, AttributeError) as err:
-        print(err)
         error_msg = 'Please provide a task name and its frequency (in hours). Example: name=eat pizza freq=3'
         return jsonify({'text': error_msg})
 
@@ -42,7 +42,11 @@ def new_task():
         task = Task(**attrs)
         task.save()
         time_unit = 'hours' if frequency > 1 else 'hour'
-        return jsonify({'text': f':ok_hand: Got it! I\'ll remind you to {name} every {frequency} {time_unit}'})
+        return jsonify(
+            {
+                'text': f':ok_hand: Got it! I\'ll remind you to {name} every {frequency} {time_unit}'
+            }
+        )
     except:
         return jsonify({'text': ':thinking_face: hmm... something went wrong. Can you try again later?'})
 
@@ -50,32 +54,78 @@ def new_task():
 def task_status():
     task_name = request.form['text']
     try:
+        if not task_name:
+            raise DoesNotExist
         task = Task.objects.get(name=task_name)
-        return jsonify({'text': f'found a task! {task.name}'})
+        return jsonify({'text': task.task_report()})
+    except DoesNotExist:
+        # couldn't find that task. get all tasks
+        all_tasks = '\n'.join([t.task_report() for t in Task.objects])
+        return jsonify({'text': f':thinking_face: hmmm...I couldn\'t find that task. Here are all of the tasks\n {all_tasks}'})
+
+@app.route('/new_event', methods=['POST'])
+def new_event():
+    '''
+    create a new event for a task.
+    expected format:
+        task(str): the name of the task
+        time(str): the time of the task 
+    '''
+
+    try:
+        # try to set the attributes
+        text = request.form['text']
+        regex = 'task=(?P<task>.*)time=(?P<time>.*)'
+        match = re.search(regex, text)
+        task_name, time_str = match.group('task').strip(), match.group('time')
+        time = dateparser.parse(time_str)
+        # if user entered an invalid time, reject
+        if time_str is not None and time is None:
+            return jsonify({'text': ':thinking_face: I didn\'t understand that time. Please correct it and try again.'})
+    except (ValueError, AttributeError) as err:
+        error_msg = 'Please provide an event and the time it happened. Example: task=eat pizza time=12:00'
+        return jsonify({'text': error_msg})
+
+    # pull the task from the database
+    try:
+        task = Task.objects.get(name=task_name)
     except DoesNotExist:
         return jsonify({'text': ':thinking_face: hmmm...I couldn\'t find that task.'})
 
-# @app.route('/new_event')
-# def new_event():
-#     pass
+    # create the event and add it
+    task.events.append(time)
+    task.save()
+    return jsonify({'text': f':pencil: got it! I added an event for {task.name} at {time:%m/%d/%Y %H:%M}'})
 
-# # routes:   
-#     # new task :task_name
-#     # new event :task_name
-#     # :task_name status
-#     # help
-# if re.search(r'^new task', msg_txt):
-#     # add new task
-#     pass
-# elif re.search(r'^new event', msg_txt):
-#     # add new event
-#     pass
-# elif 'status' in msg_txt:
-#     # get status of a task
-#     pass
-# else:
-#     # help!
-# pass
+@app.route('/remove_task', methods=['POST'])
+def remove_task():
+    '''
+    remove an event
+    '''
+    task_name = request.form['text']
+    try:
+        task = Task.objects.get(name=task_name)
+        task.delete()
+        return jsonify({'text': f':boom: remove task {task.name}'})
+    except DoesNotExist:
+        return jsonify({'text': f':thinking_face: could not find task {task.name}.'})
+
+    pass
+
+# def _task_report(task):
+#     '''
+#     task(Task object)
+#     return string reporting on the tasks last events
+#     example: f'{task.name}: last done @ {last_event:%m/%d/%Y %H:%M:%S}'
+#     or 
+#     f'{task.name}: last done never'
+#     '''
+
+#     if task.events:
+#         last_event = task.events[-1]
+#         return f'{task.name}: last done @ {last_event:%m/%d/%Y %H:%M:%S}'
+#     else:
+#         return f'{task.name}: last done never'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
--- models.py
+++ models.py
@@ -1,14 +1,6 @@
 from mongoengine import Document, StringField, IntField, DateTimeField, ListField, ReferenceField
 import datetime
 
-class Event(Document):
-    '''
-    fields:
-        date_modified(datetime): the timestamp of this event
-
-    '''
-    date_modified = DateTimeField(default=datetime.datetime.now)
-    
 class Task(Document):
     '''
     fields:
@@ -21,4 +13,42 @@ class Task(Document):
     frequency = IntField(required=True)
     quantity = IntField()
     units = StringField()
-    events = ListField(ReferenceField(Event))
+    events = ListField(DateTimeField(default=datetime.datetime.now))
+
+    def last_event(self):
+        '''
+        returns them most recent event
+        if no events return None
+        '''
+        if self.events:
+            self.events.sort()
+            return self.events[-1]
+
+    def task_report(self):
+        '''
+        return string reporting on the tasks last events
+        example: f'{self.name}: last done @ {last_event:%m/%d/%Y %H:%M:%S}'
+        or 
+        f'{self.name}: last done never'
+        '''
+
+        latest_event = self.last_event()
+
+        if self.is_due():
+            due_alert = ':rotating_light: IS DUE'
+        else:
+            due_alert = 'is not due'
+
+        if latest_event is not None:
+            return f'{self.name}: last done @ {latest_event:%m/%d/%Y %H:%M} and {due_alert}'
+        else:
+            return f'{self.name}: last done never and {due_alert}'
+
+    def is_due(self):
+        '''
+        returns if this task is now due
+        '''
+
+        last_done = self.last_event()
+        hours_since_last_event = (datetime.datetime.now() - last_done).seconds / 3600
+        return hours_since_last_event >= self.frequency
