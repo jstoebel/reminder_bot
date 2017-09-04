ae61a5f02116aed1012443e4bfece02a8b3b73b6
Jacob Stoebel <jstoebel@gmail.com>
2017-09-04 12:52:59 -0400

    finish tests for task model

--- tests/test_task.py
+++ tests/test_task.py
@@ -8,7 +8,7 @@ from factories import TaskFactory
 import pytest
 from mongoengine import ValidationError
 
-import dateparser
+from dateparser import parse
 import json
 
 class TestTask(test_base.TestBase):
@@ -25,6 +25,9 @@ class TestTask(test_base.TestBase):
 
     @pytest.mark.parametrize("attr", test_data)
     def test_required_attrs(self, attr):
+        '''
+        test all attrs that are required
+        '''
         t = TaskFactory.build()
         setattr(t, attr, None)
 
@@ -33,27 +36,69 @@ class TestTask(test_base.TestBase):
         assert e.value.message == f'ValidationError (Task:None) (Field is required: [\'{attr}\'])'
 
     def test_last_event(self):
+        '''
+        test that the latest event is the most recent not last in the list
+        '''
         t = TaskFactory.create()
 
         # 2am and 1am
-        events = [dateparser.parse(f'{hour}:00') for hour in range(2,0,-1)]
+        events = [parse(f'{hour}:00') for hour in range(2,0,-1)]
         t.events = events
         t.save()
         assert t.last_event() == events[0]
 
-
-        # for hour in range(2,1,-1):
-        #     event = dateparser.parse(f'{hour}:00')
-        #     t.events.append(event)
-        # t.events.append(dateparser.parse('10:00am'))
-        # t.events.append
-
     def test_last_event_none(self):
-        pass
+        '''
+        returns None when no events present
+        '''
+        t = TaskFactory.create()
+        assert t.last_event() is None
 
-    def test_task_report(self):
-        pass
+    def test_is_due_no_events(self):
+        '''
+        should be due when no events present
+        '''
+        t = TaskFactory.create()
+        assert t.is_due()
 
+    def test_is_due(self):
+        t = TaskFactory.create()
+        last_event = parse(f'{t.frequency} hours ago')
+        t.events.append(last_event)
+        t.save()
+        assert t.is_due()
+
+    def test_not_due(self):
+        '''
+        not due when an event within the frequency
+        '''
+        t = TaskFactory.create()
+        last_event = parse(f'{t.frequency - 1} hours ago')
+        t.events.append(last_event)
+        t.save()
+        assert not t.is_due()
+
+    def test_report(self):
+        '''
+        task has an event and is due 
+        '''
+
+        t = TaskFactory.create()
+        last_event = parse(f'{t.frequency} hours ago')
+        t.events.append(last_event)
+        t.save()
+        assert t.task_report() == f'{t.name}: last done @ {last_event:%m/%d/%Y %H:%M} and :rotating_light: IS DUE'
+
+    def test_report_no_event(self):
+        t = TaskFactory.create()
+        assert t.task_report() == f'{t.name}: last done never and :rotating_light: IS DUE'
+
+    def test_report_not_due(self):
+        t = TaskFactory.create()
+        last_event = parse(f'{t.frequency - 1} hours ago')
+        t.events.append(last_event)
+        t.save()
+        assert t.task_report() == f'{t.name}: last done @ {last_event:%m/%d/%Y %H:%M} and is not due'
 
 if __name__ == '__main__':
     unittest.main()
